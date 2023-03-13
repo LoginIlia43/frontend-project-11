dd4d06663f07f19c2e675e34195281f346f7743d
Ilia Loginov <loginoviv43@gmail.com>
2023-03-13 15:31:43 +0100

    add error in parser

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,11 @@
 export default (html) => {
   const parse = new DOMParser();
-  const result = parse.parseFromString(html, 'application/xml');
+  const result = parse.parseFromString(html, 'application/xml').querySelector('rss');
 
   return new Promise((resolve, reject) => {
+    if (!result) {
+      reject(new Error('link doesn\'t contain a valid RSS'));
+    }
     resolve(result);
   });
 };
