178f7b76d5ee6cbf068318b7f44f73eabcf851bc
Ilia Loginov <loginoviv43@gmail.com>
2023-03-22 17:58:16 +0100

    add renderLinkAttr and edit css

--- src/watcherUi.js
+++ src/watcherUi.js
@@ -1,4 +1,15 @@
 import onChange from 'on-change';
 
-export default (state) => onChange(state, (path, value, prev) => {
+export default (wUiState) => onChange(wUiState, (path, value, prev) => {
+    const entries = Object.entries(wUiState.idIsRead);
+    const posts = Array.from(document.querySelectorAll('a'));
+    entries.forEach(([ id, isRead ]) => {
+        const post = posts.find((post) => post.getAttribute('data-id') === id);
+        if (isRead) {
+            post.classList.remove('fw-bold');
+            post.classList.add('fw-normal');
+        } else {
+            post.classList.add('fw-bold');
+        }
+    });
 });
